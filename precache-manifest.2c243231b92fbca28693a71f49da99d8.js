c8413c00a3db59c428b46c7abb3ad416a4e1606f
dilapitan <domlapitan@gmail.com>
2022-05-01 08:34:39 +0800

    Deploy

---
+++ precache-manifest.2c243231b92fbca28693a71f49da99d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fd07d82396b900f168ae",
+    "url": "/practice-saa/css/chunk-vendors.d58f19cc.css"
+  },
+  {
+    "revision": "75308ffc55cad10b741daad14780b3ae",
+    "url": "/practice-saa/index.html"
+  },
+  {
+    "revision": "3dfb75361909b834b08e",
+    "url": "/practice-saa/js/app.c551293a.js"
+  },
+  {
+    "revision": "fd07d82396b900f168ae",
+    "url": "/practice-saa/js/chunk-vendors.5af59ba9.js"
+  },
+  {
+    "revision": "35c65c7e7b797246c4e77ea292328bc3",
+    "url": "/practice-saa/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/practice-saa/robots.txt"
+  }
+]);
